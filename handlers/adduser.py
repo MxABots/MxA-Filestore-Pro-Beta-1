f72b2686e2423ae3eaaff6396d7795e501f93059
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-12 19:30:21 +0530

    Update adduser.py

--- handlers/adduser.py
+++ handlers/adduser.py
@@ -13,3 +13,23 @@ async def adduser(Client, message: Message):
                 int(LOG_CHANNEL),
                 f"#new_user :-\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) started @{BOT_USERNAME}!"
             )
+
+
+async def start(client, message):
+    await handle_private_message(client, message)
+    await message.delete()
+    fsub = await force_sub(client, message)
+    if fsub == 400:
+        return
+    await message.reply_text(
+        START_TEXT.format(message.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Dev 👨‍💻", user_id="6112935306"),
+                    InlineKeyboardButton("Close ❌", callback_data="delete")
+                ]
+            ]
+        )
+    )
