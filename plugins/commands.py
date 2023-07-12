5a89e7a4c8bc3b3e25f5561f9a0af56e0ca49982
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-12 19:34:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,5 +1,6 @@
 from bot import mxabot
-from handlers.adduser import *
+from handlers.adduser import adduser
+from handlers.start import start
 from pyrogram import Client, filters
 from pyrogram.types import (
     InlineKeyboardMarkup,
@@ -15,22 +16,4 @@ async def handle_private_message(client: Client, message: Message):
     await adduser(client, message)
 
 @mxabot.on_message(filters.command('start'))
-async def start(client, message):
-    await handle_private_message(client, message)
-    await message.delete()
-    fsub = await force_sub(client, message)
-    if fsub == 400:
-        return
-    await message.reply_text(
-        START_TEXT.format(message.from_user.mention),
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("Dev 👨‍💻", user_id="6112935306"),
-                    InlineKeyboardButton("Close ❌", callback_data="delete")
-                ]
-            ]
-        )
-    )
-
+    await start(client, message):
