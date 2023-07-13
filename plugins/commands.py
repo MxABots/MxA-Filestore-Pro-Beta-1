75d4521038f13a9fabd22916fa3a2458c7e9cf5e
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-13 22:34:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -16,5 +16,21 @@ async def handle_private_message(client: Client, message: Message):
     await adduser(client, message)
 
 @mxabot.on_message(filters.command('start'))
-async def st(client, message):
-    await start(client, message):
+async def start(client, message):
+    await handle_private_message(client, message)
+    await message.delete()
+    fsub = await force_sub(client, message)
+    if fsub == 400:
+        return
+    await message.reply_text(
+        START_TEXT.format(message.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Dev 👨‍💻", user_id="6112935306"),
+                    InlineKeyboardButton("Close ❌", callback_data="delete")
+                ]
+            ]
+        )
+    )
