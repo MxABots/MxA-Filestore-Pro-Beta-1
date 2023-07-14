b1b4b5bd5bc1e2cc80eb0285e9af87e0b6346add
iSmartBoi Ujjwal <138148484+MxABots@users.noreply.github.com>
2023-07-14 21:00:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,9 +8,9 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyromod import listen
 from pyrogram import Client
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("API_ID", "28376552"))
+API_HASH = getenv("API_HASH", "d4ead25e66fc1c4326d533614dad3ecd")
+BOT_TOKEN = getenv("BOT_TOKEN", "6399715419:AAEiy2g3YfsdYO2KHd8yC6qy1dNfKrtxg0s")
 
 class mxabot(Client):
     def main():
